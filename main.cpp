f06b958ac1f71fbc58826e6ac5dcd3db87f4c6b0
SamuilYu <sam_yusufov@mail.ru>
2022-05-13 17:24:17 +0300

    add iteration parameters and results

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ void collectMetrics(int i, int j) {
     auto cond = std::make_shared<SchedulingConditions>(*dp);
     std::shared_ptr<Solution> probe = std::make_shared<Schedule>(*cond);
     std::ostringstream par;
-    par << "C:\\Users\\samyu\\CLionProjects\\SA-Schefuling-With-Resource/data/parameters/parameters_"<< i << ".json";
+    par << "C:\\Users\\samyu\\CLionProjects\\SA-Schefuling-With-Resource/data/parameters/iterations/iterations_parameters_"<< i << ".json";
     auto sa = ParametersParser().parse(par.str(), cond, probe);
     auto schedule = std::make_shared<Schedule>(*cond);
     auto maxMilli = 0L;
@@ -29,7 +29,7 @@ void collectMetrics(int i, int j) {
         minMilli = milli < minMilli? milli: minMilli;
     }
     std::ostringstream res;
-    res << "C:\\Users\\samyu\\CLionProjects\\SA-Schefuling-With-Resource/data/results/result" << i<< "_" << j <<".txt";
+    res << "C:\\Users\\samyu\\CLionProjects\\SA-Schefuling-With-Resource/data/results/iterations/iterations_result" << i<< "_" << j <<".txt";
     std::ofstream result;
     result.open(res.str());
     result << minAnswer << " " << maxAnswer << " " << maxAnswer/minAnswer << " " << minMilli << " " << maxMilli;
@@ -40,10 +40,23 @@ void collectMetrics(int i, int j) {
 int main() {
     std::cout << std::thread::hardware_concurrency() << std::endl;
     std::vector<int> dags = {471, 423, 455, 310, 480, 280, 387, 142, 213, 143, 244, 250, 66, 483, 281, 282, 484, 316, 334, 67};
-    std::vector<int> easy = {213, 244, 282, 316, 334, 484, 423, 455};
-    std::vector<int> hard = {66, 281,310,387,471,483};
-    std::vector<int> medium = {67,143,142,250,280,480};
+    std::vector<int> easy = {224, 420, 63,86,303,466, 140};
+    std::vector<int> hard = {402,33,34,499,8,36};
+    std::vector<int> medium = {193,95,480,440,353,198,287};
     std::vector<std::thread> pool;
+//    for (int j = 0; j < 500; j++) {
+//        for (int i = 0; i < 5; i++) {
+//            if (pool.size() < std::thread::hardware_concurrency()) {
+//                pool.emplace_back(&collectMetrics, i, j);
+//            }
+//        }
+//        for (auto& th: pool) {
+//            if (th.joinable()) {
+//                th.join();
+//            }
+//        }
+//        pool.clear();
+//    }
     for (int i = 0; i < 5; i++) {
         for (auto each: easy) {
             if (pool.size() < std::thread::hardware_concurrency()) {
