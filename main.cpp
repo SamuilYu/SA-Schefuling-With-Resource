f25ebca7730b51cae8b901895f21c08dcb8564da
SamuilYu <sam_yusufov@mail.ru>
2022-05-17 11:37:29 +0300

    final

--- main.cpp
+++ main.cpp
@@ -3,21 +3,20 @@
 #include "Parsers/ParametersParser.h"
 #include "Parsers/DependencyGraphParser.h"
 #include "UniprocessorSchedulingWithResource/Schedule.h"
-#include "filesystem"
 
-int main() {
+int main(int agrc, char* argv[]) {
     std::ostringstream dag;
-    dag << std::filesystem::current_path() << "\\data\\graphs\\dag0.txt";
+    dag << "C:\\Users\\samyu\\CLionProjects\\SA-Schefuling-With-Resource\\data\\trees\\tree_9.txt";
     auto dp = DependencyGraphParser::parse(dag.str());
     auto cond = std::make_shared<SchedulingConditions>(*dp);
     std::shared_ptr<Solution> probe = std::make_shared<Schedule>(*cond);
     std::ostringstream par;
-    par << std::filesystem::current_path() << "\\parameters\\main\\main_1.json";
+    par << "C:\\Users\\samyu\\CLionProjects\\SA-Schefuling-With-Resource\\data\\parameters\\main\\main_2.json";
     auto sa = ParametersParser().parse(par.str(), cond, probe);
     auto schedule = std::make_shared<Schedule>(*cond);
     auto answer = sa->Start(schedule);
     std::ostringstream res;
-    res << std::filesystem::current_path() << "\\data\\results\\main\\result.txt";
+    res << "C:\\Users\\samyu\\CLionProjects\\SA-Schefuling-With-Resource\\data\\results\\result.txt";
     std::ofstream result;
     result.open(res.str());
     result << answer << "\n";
